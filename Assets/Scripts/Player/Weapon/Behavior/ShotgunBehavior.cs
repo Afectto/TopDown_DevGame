69b695fc7564c9ba4e407fa026c6622db5beeb26
Afectto <138150311+Afectto@users.noreply.github.com>
2024-07-19 17:41:36 +0700

    Fix Shotgun

--- Assets/Scripts/Player/Weapon/Behavior/ShotgunBehavior.cs
+++ Assets/Scripts/Player/Weapon/Behavior/ShotgunBehavior.cs
@@ -2,19 +2,27 @@
 
 public class ShotgunBehavior : AbstractWeaponBehavior
 {
+    private const float ShotAngle = 10f;
+    private const int BulletCount = 5;
+
     public ShotgunBehavior(float damage, Transform firePoint, Bullet bulletPrefab) : base(damage, firePoint, bulletPrefab)
     {
     }
 
     public override void Shoot(Vector3 dir)
     {
-        for(int i = 0; i < 5; i++)
+        float halfAngle = ShotAngle / 2f;
+        float angleStep = ShotAngle / (BulletCount - 1);
+
+        for (int i = 0; i < BulletCount; i++)
         {
-            float angle = (i - 2) * 10f;
-            Quaternion rotation = Quaternion.Euler(0, angle, 0);
-            var bullet = CreateBullet(rotation, dir);
+            float angle = -halfAngle + i * angleStep;
+            Quaternion rotation = Quaternion.Euler(0, 0, angle);
+
+            Vector2 shotDirection = rotation * dir;
+            var bullet = CreateBullet(rotation, shotDirection);
             bullet.transform.localScale = Vector3.one * 0.75f;
         }
     }
-
+    
 }
---
+++ Assets/Scripts/Player/Weapon/Bullet/ShotgunBullet.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunBullet : Bullet
+{
+    private Vector2 startPoint;
+    private void Awake()
+    {
+        startPoint = transform.position;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if (Vector2.Distance(startPoint, transform.position) > 7)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
